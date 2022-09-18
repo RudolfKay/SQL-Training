f184eefc072f23f91e1c7b6a93f3630d532d5107
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-09-18 23:59:59 +0300

    SQL-Movies Test-04 Completed

--- sql-movies/test/04-create-releationship-tables.test.ts
+++ sql-movies/test/04-create-releationship-tables.test.ts
@@ -6,7 +6,11 @@ import {
   MOVIE_DIRECTORS,
   MOVIE_PRODUCTION_COMPANIES,
   MOVIES,
-  GENRES
+  GENRES,
+  ACTORS,
+  DIRECTORS,
+  KEYWORDS,
+  PRODUCTION_COMPANIES
 } from "../src/table-names";
 import { Database } from "../src/database";
 import { tableInfo } from "../src/queries/table-info";
@@ -15,28 +19,47 @@ import { minutes, Log } from "./utils";
 const CREATE_MOVIE_GENRES_TABLE = `CREATE table ${MOVIE_GENRES}(
   movie_id integer NOT NULL,
   genre_id integer NOT NULL,
-  PRIMARY KEY(movie_id, genre_id),
-  FOREIGN KEY(movie_id) REFERENCES ${MOVIES}(id)
+  PRIMARY KEY (movie_id, genre_id),
+  FOREIGN KEY (movie_id) REFERENCES ${MOVIES}(id)
   ON DELETE CASCADE,
   FOREIGN KEY (genre_id) REFERENCES ${GENRES}(id)
   )`;
 
 const CREATE_MOVIE_ACTORS_TABLE = `CREATE table ${MOVIE_ACTORS}(
   movie_id integer NOT NULL,
-  genre_id integer NOT NULL,
+  actor_id integer NOT NULL,
+  PRIMARY KEY(movie_id, actor_id),
+  FOREIGN KEY(movie_id) REFERENCES ${MOVIES}(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (actor_id) REFERENCES ${ACTORS}(id)
 )`;
 
 const CREATE_MOVIE_DIRECTORS_TABLE = `CREATE table ${MOVIE_DIRECTORS}(
-  
+  movie_id integer NOT NULL,
+  director_id integer NOT NULL,
+  PRIMARY KEY(movie_id, director_id),
+  FOREIGN KEY(movie_id) REFERENCES ${MOVIES}(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (director_id) REFERENCES ${DIRECTORS}(id)
 )`;
 
 const CREATE_MOVIE_KEYWORDS_TABLE = `CREATE table ${MOVIE_KEYWORDS}(
-  
-  )`;
+  movie_id integer NOT NULL,
+  keyword_id integer NOT NULL,
+  PRIMARY KEY(movie_id, keyword_id),
+  FOREIGN KEY(movie_id) REFERENCES ${MOVIES}(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (keyword_id) REFERENCES ${KEYWORDS}(id)
+)`;
 
 const CREATE_MOVIE_PRODUCTION_COMPANIES_TABLE = `CREATE table ${MOVIE_PRODUCTION_COMPANIES}(
-  
-  )`;
+  movie_id integer NOT NULL,
+  company_id integer NOT NULL,
+  PRIMARY KEY(movie_id, company_id),
+  FOREIGN KEY(movie_id) REFERENCES ${MOVIES}(id)
+  ON DELETE CASCADE,
+  FOREIGN KEY (company_id) REFERENCES ${PRODUCTION_COMPANIES}(id)
+)`;
 
 describe("Insert Combined Data", () => {
   let db: Database;
