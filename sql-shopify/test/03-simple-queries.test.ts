803d498a843ac94ad236729f2cabea8d88a04d22
Rūdolfs K <8761347+RudolfKay@users.noreply.github.com>
2022-09-20 20:42:24 +0300

    SQL-Shopify Test 03

--- sql-shopify/test/03-simple-queries.test.ts
+++ sql-shopify/test/03-simple-queries.test.ts
@@ -9,7 +9,8 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select app count with rating of 5 stars", async done => {
-        const query = `todo`;
+        const query = `SELECT COUNT(*) AS count FROM apps
+            WHERE (rating = 5)`;
         const result = await db.selectSingleRow(query);
         expect(result).toEqual({
             count: 731
@@ -18,7 +19,10 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select top 3 develepors with most apps published", async done => {
-        const query = `todo`;
+        const query = `SELECT COUNT(*) AS count, developer FROM apps
+            GROUP BY developer
+            ORDER BY count DESC
+            LIMIT 3`;
 
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
@@ -30,7 +34,10 @@ describe("Simple Queries", () => {
     }, minutes(1));
 
     it("should select count of reviews created in year 2014, 2015 and 2016", async done => {
-        const query = `todo`;
+        const query = `SELECT substr(date_created, 7, 4) AS year, COUNT(*) AS review_count FROM reviews
+            WHERE year IN ('2014', '2015', '2016')
+            GROUP BY year
+            ORDER BY year ASC`;
         const result = await db.selectMultipleRows(query);
         expect(result).toEqual([
             { year: "2014", review_count: 6157 },
